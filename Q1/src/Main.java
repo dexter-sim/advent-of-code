15ed911fc913bfab1a7d3a155b3d068188da9192
Dexter <dextersimch@gmail.com>
2021-12-01 14:25:07 +0800

    q1 program

---
+++ Q1/src/Main.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        part1();
+        part2();
+    }
+
+    public static void part1() throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader("q1-inputs.txt"));
+        String line = br.readLine();
+        int prev = Integer.parseInt(line);
+        int count = 0;
+        while (line != null){
+            int curr = Integer.parseInt(line);
+            if (curr > prev){
+                count++;
+            }
+            prev = curr;
+            line = br.readLine();
+        }
+        System.out.println(count);
+    }
+
+    public static void part2() throws IOException {
+        ArrayList<Integer> list = new ArrayList<>();
+        BufferedReader reader = new BufferedReader(new FileReader("q1-inputs.txt"));
+        String s = reader.readLine();
+        while (s != null){
+            list.add(Integer.parseInt((s)));
+            s = reader.readLine();
+        }
+        int count = 0;
+        for (int i = 3; i < list.size(); i++){
+            int prev = list.get(i - 3) + list.get(i - 2) + list.get(i - 1);
+            int curr = list.get(i - 2) + list.get(i - 1) + list.get(i);
+            if (curr > prev){
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # advent-of-code-2021
+
+My programs to solve questions from Advent of Code 2021.
